3b2f60adfd80b1dd3224c6ff006816c40c0c64a0
bodand <bodand@proton.me>
2023-06-03 21:34:45 +0200

    feat: add Class
    - add Class for representing a Class
    - link HFPD to libHF
    - add tests for Class
    
    Signed-off-by: bodand <bodand@proton.me>

---
+++ libHF/Classes/Class.fs
@@ -0,0 +1,13 @@
+﻿namespace libHF.Classes
+
+
+type Class(name: string) =
+    let mutable homeworks = List<Homework>.Empty
+
+    member this.Homeworks
+        with get (): List<Homework> = homeworks
+        and private set value = homeworks <- value
+
+    member val Name = name with get
+
+    member this.AddHomework(hw) = this.Homeworks <- hw :: this.Homeworks
---
+++ libHFTests/Classes/ClassTest.fs
@@ -0,0 +1,36 @@
+﻿namespace libHFTests.Classes
+
+open Xunit
+open libHF.Classes
+open libHF.Student
+
+module ClassTests =
+    let students =
+        [ Student("John", Neptun "UVWXYZ")
+          Student("Foo Bar", Neptun "123456") ]
+
+    let grading = [ PointGuideline("OO design", 2); ErrataGuideline("typecheck", 4) ]
+    
+    [<Fact>]
+    let ``Class can be constructed by its name`` () =
+        let exc = Record.Exception(fun() -> Class("Test") |> ignore)
+        Assert.Null exc
+        
+    [<Fact>]
+    let ``Class constructor sets name`` () =
+        let klass = Class("Test")
+        Assert.Equal("Test", klass.Name)
+        
+    [<Fact>]
+    let ``Fresh Class object does not have homework`` () =
+        let klass = Class("Test")
+        Assert.Empty klass.Homeworks
+        
+    [<Fact>]
+    let ``Homework can be added to Class object`` () =
+        let klass = Class("Test")
+        Assert.Empty klass.Homeworks
+        
+        let homework = Homework(students, grading)
+        klass.AddHomework(homework)
+        Assert.Contains (homework, klass.Homeworks)
