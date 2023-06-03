ae7375eda944fd1cfafe6ea2d32000f5bd62487c
bodand <bodand@proton.me>
2023-06-03 17:20:33 +0200

    feat: add Grading
    - add Grading the concrete value of the GradingGuideline defined points
    
    Signed-off-by: bodand <bodand@proton.me>

---
+++ libHF/Submissions/Grading.fs
@@ -0,0 +1,13 @@
+﻿namespace libHF.Submissions
+
+open System
+open libHF.Classes
+
+type Grading(guideline: GradingGuideline, received: int) =
+    do
+        if not <| guideline.AllowedPoints received then
+            raise
+            <| ArgumentException $"Illegal amount of points received on grade: {received}"
+
+    member val Guideline = guideline with get
+    member val ReceivedPoints = received with get
---
+++ libHFTests/Submissions/GradingTests.fs
@@ -0,0 +1,42 @@
+﻿namespace libHFTests.Submissions
+
+open System
+open Xunit
+open libHF.Submissions
+open libHF.Classes
+
+module GradingTests =
+    let point = PointGuideline("OO design", 2)
+    let errata = ErrataGuideline("typecheck", -2)
+
+    [<Fact>]
+    let ``Grading can be constructed with valid positive points`` () =
+        let exc = Record.Exception(fun () -> Grading(point, 2) |> ignore)
+        Assert.Null exc
+
+    [<Fact>]
+    let ``Grading can be constructed with valid negitve points`` () =
+        let exc = Record.Exception(fun () -> Grading(errata, -2) |> ignore)
+        Assert.Null exc
+
+    [<Fact>]
+    let ``Grading throws if to be constructed with invalid positive points`` () =
+        let exc = Record.Exception(fun () -> Grading(errata, 2) |> ignore)
+        Assert.NotNull exc
+        Assert.IsType<ArgumentException> exc |> ignore
+
+    [<Fact>]
+    let ``Grading throws if to be constructed with invalid negitve points`` () =
+        let exc = Record.Exception(fun () -> Grading(point, -2) |> ignore)
+        Assert.NotNull exc
+        Assert.IsType<ArgumentException> exc |> ignore
+
+    [<Fact>]
+    let ``Grading constructor sets received points`` () =
+        let gr = Grading(point, 2)
+        Assert.Equal(2, gr.ReceivedPoints)
+
+    [<Fact>]
+    let ``Grading constructor sets the graded Guideline`` () =
+        let gr = Grading(point, 2)
+        Assert.Same(point, gr.Guideline)
