14214722eabed8c7c167fe477f2cc80c45b8f8f0
bodand <bodand@proton.me>
2023-06-03 14:47:41 +0200

    feat(libHF): add GradingGuideline
    - add GradingGuideline sum type
    - add tests for previous
    - add GradingGuideline.AllowedPoints that checks whether a point is valid for a GradingGuideline
    
    Signed-off-by: bodand <bodand@proton.me>

---
+++ libHF/Classes/GradingGuideline.fs
@@ -0,0 +1,32 @@
+﻿namespace libHF.Classes
+
+open System
+
+/// <summary>
+/// A sum-type for the types of grading that results in points.
+/// </summary>
+/// <remarks>
+/// Grading is a sum of positive and negative points.
+/// Positive points are given for particular pieces of the answers that are expected to be present,
+/// while negative points are for parts of code/answer that are so wrong, that they should be punished.
+/// A Homework defines a set of Guidelines which together make up the points that may be gathered for that
+/// homework with the errata that may be encountered.
+/// </remarks>
+type GradingGuideline =
+    /// <summary>
+    /// The positive grading object.
+    /// </summary>
+    /// <param name="name">The textual description of the points that can be gathered.</param>
+    /// <param name="max">The maximum number of points that can be given for this grading guideline.</param>
+    | PointGuideline of name: string * max: int
+    /// <summary>
+    /// The negative grading object.
+    /// </summary>
+    /// <param name="name">The textual description of why these point are subtracted.</param>
+    /// <param name="max">The maximum number (maximum in absolute value) of points that can be subtracted for this errata.</param>
+    | ErrataGuideline of name: string * max: int
+
+    member this.AllowedPoints (x: int): bool =
+        match this with
+        | PointGuideline(name, max) -> 0 <= x && x <= max
+        | ErrataGuideline(name, max) -> -Math.Abs(max) <= x && x <= 0
---
+++ libHFTests/Classes/GradingGuidelineTests.fs
@@ -0,0 +1,66 @@
+﻿namespace libHFTests.Classes
+
+open Xunit
+open libHF.Classes
+
+module GradingGuidelineTests =
+    [<Fact>]
+    let ``A positive grading guideline can be constructed from a string and int`` () =
+        let gr = PointGuideline("OO design", 1)
+        Assert.NotNull gr
+
+    [<Fact>]
+    let ``A negative grading guideline can be constructed from a string and int`` () =
+        let gr = ErrataGuideline("typecheck", 4)
+        Assert.NotNull gr
+
+    [<Fact>]
+    let ``A positive grading guideline reports zero value as accepted`` () =
+        let gr = PointGuideline("OO design", 1)
+        Assert.True <| gr.AllowedPoints 0
+
+    [<Fact>]
+    let ``A positive grading guideline reports a non-edge-case value as accepted`` () =
+        let gr = PointGuideline("OO design", 2)
+        Assert.True <| gr.AllowedPoints 1
+        
+    [<Fact>]
+    let ``A positive grading guideline reports a upper-edge case value as accepted`` () =
+        let gr = PointGuideline("OO design", 2)
+        Assert.True <| gr.AllowedPoints 2
+
+    [<Fact>]
+    let ``A positive grading guideline reports a negative value as not accepted`` () =
+        let gr = PointGuideline("OO design", 2)
+        Assert.False <| gr.AllowedPoints -2
+        
+    [<Fact>]
+    let ``A positive grading guideline reports a too great value as not accepted`` () =
+        let gr = PointGuideline("OO design", 2)
+        Assert.False <| gr.AllowedPoints 42
+
+    [<Fact>]
+    let ``A negative grading guideline reports zero value as accepted`` () =
+        let gr = ErrataGuideline("typecheck", 1)
+        Assert.True <| gr.AllowedPoints 0
+
+    [<Fact>]
+    let ``A negative grading guideline reports a non-edge-case value as accepted`` () =
+        let gr = ErrataGuideline("typecheck", 2)
+        Assert.True <| gr.AllowedPoints -1
+        
+    [<Fact>]
+    let ``A negative grading guideline reports a upper-edge case value as accepted`` () =
+        let gr = ErrataGuideline("typecheck", 2)
+        Assert.True <| gr.AllowedPoints -2
+
+    [<Fact>]
+    let ``A negative grading guideline reports a positive value as not accepted`` () =
+        let gr = ErrataGuideline("typecheck", 2)
+        Assert.False <| gr.AllowedPoints 3
+        
+    [<Fact>]
+    let ``A negative grading guideline reports a too great value as not accepted`` () =
+        let gr = ErrataGuideline("typecheck", 2)
+        Assert.False <| gr.AllowedPoints -42
+
