563dd3050164f73891fb0e19bc13e95d9794c195
bodand <bodand@proton.me>
2023-06-13 17:32:34 +0200

    feat(HFPDd): add ServerImplementation helper

---
+++ HFPDd.Tests/ServerImplementationTests.fs
@@ -0,0 +1,61 @@
+module Tests
+
+open System
+open System.IO
+open System.Threading.Tasks
+open HFPDd
+open Xunit
+
+[<Fact>]
+let ``ServerImplementation's Run waits for Task`` () =
+    use strm = new MemoryStream()
+
+    use server =
+        new ServerImplementation(
+            task {
+                let buf: byte[] = Array.create 999 69uy
+                do! Task.Delay 350
+                let! read = strm.WriteAsync(buf)
+                return read
+            },
+            { new IDisposable with
+                member this.Dispose() = () }
+        )
+
+    server.Run()
+    Assert.All(strm.GetBuffer(), Action<byte>(fun byt -> Assert.Equal(byt, 69uy)))
+
+[<Fact>]
+let ``ServerImplementation's Dispose waits for Task`` () =
+    use strm = new MemoryStream()
+
+    let server : IDisposable =
+        new ServerImplementation(
+            task {
+                let buf: byte[] = Array.create 999 69uy
+                do! Task.Delay 350
+                let! read = strm.WriteAsync(buf)
+                return read
+            },
+            { new IDisposable with
+                member this.Dispose() = () }
+        )
+    server.Dispose()
+        
+    Assert.All(strm.GetBuffer(), Action<byte>(fun byt -> Assert.Equal(byt, 69uy)))
+
+[<Fact>]
+let ``ServerImplementation's Dispose calls inner Dispose`` () =
+    let mutable called = 0
+
+    let server: IDisposable =
+        new ServerImplementation(
+            task {
+                do! Task.Delay 350
+            },
+            { new IDisposable with
+                member this.Dispose() = called <- called + 1; () }
+        )
+    server.Dispose()
+        
+    Assert.Equal(called, 1)
---
+++ HFPDd/ServerImplementation.fs
@@ -0,0 +1,26 @@
+﻿namespace HFPDd
+
+open System
+open System.Threading.Tasks
+
+/// <summary>
+/// Helper class that wraps a task and an IDisposable object.
+/// This is returned by the TcpServer implementation and serves as the
+/// main server object for HFPDd.
+/// </summary>
+/// <remarks>
+/// <para>
+/// The class can be disposed through the IDisposable interface, but note that
+/// this will also wait for the given task to finish.
+/// </para>
+/// </remarks>
+type ServerImplementation(wait: Task<unit>, server: IDisposable) =
+    interface IDisposable with
+        member this.Dispose() =
+            wait.Wait()
+            server.Dispose()
+
+    /// <summary>
+    /// Waits until the given task finishes.
+    /// </summary>
+    member this.Run() = wait.Wait()
